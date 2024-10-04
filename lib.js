d899939252f2f390d2d2a4c52cd096b51ac254a5
tuanna9414 <kojinyoji@gmail.com>
2024-10-04 22:50:36 +0700

    update

--- lib.js
+++ lib.js
@@ -114,7 +114,7 @@ async function loginAndOpenExtension(user, path) {
   }
   
   let browser = await puppeteer.launch({
-    headless: false,
+    // headless: false,
     args,
     // defaultViewport: {width: 800, height: 600, deviceScaleFactor: 2},
     // targetFilter: (target) => target.type() !== "other", // Anh huong den iframe
