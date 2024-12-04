c4c5785a556ebf7dc66be2f88a4616b084349269
tuanna9414 <kojinyoji@gmail.com>
2024-12-04 23:12:42 +0700

    update

--- dawn.js
+++ dawn.js
@@ -132,7 +132,7 @@ const run = async () => {
 				});
 				
 				if (status < 1) {
-					throw 'Solve captcha failed';
+					throw new Error('Solve captcha failed', status, data);
 				}
 				
 				console.log('Text captcha:', data);
